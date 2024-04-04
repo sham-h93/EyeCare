e953a7289804353150f49013f0adc86e08c75da7
hoseincode <hoseinsadonasl@gmail.com>
2024-04-05 03:05:02 +0330

    feat: create notification utils for service

---
+++ app/src/main/java/ir/hoseinsa/eyecareapp/utils/NotificationUtils.kt
@@ -0,0 +1,35 @@
+package ir.hoseinsa.eyecareapp.utils
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+
+object NotificationUtils {
+
+    const val CHANNEL_ID = "202020app"
+    const val CHANNEL_NAME = "20-20-20 Service notification"
+
+    fun Context.notificationBuilder(
+        title: String,
+        content: String
+    ): Notification {
+        createNotificationChannel()
+        return NotificationCompat.Builder(this, "873632")
+            .setContentTitle(title)
+            .setContentText(content)
+            .build()
+    }
+
+    private fun Context.createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance)
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+
+}
